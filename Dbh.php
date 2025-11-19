288c55b345be3e7fa12962da3750a446cf0df1bf
unknown <jamesvenzuela@gmail.com>
2025-11-19 14:47:06 +0800

    wa

--- Dbh.php
+++ Dbh.php
@@ -4,25 +4,28 @@ class Dbh {
     private $user;
     private $pwd;
     private $dbName;
+    private $port;
 
     public function __construct() {
-        // Check if running on Railway (environment variables set)
-        if (getenv('MYSQL_HOST')) {
-            $this->host = getenv('MYSQL_HOST');
-            $this->user = getenv('MYSQL_USER');
-            $this->pwd = getenv('MYSQL_PASSWORD');
-            $this->dbName = getenv('MYSQL_DATABASE');
+        // Railway MySQL sets these specific variables
+        if (getenv('MYSQLHOST')) {
+            $this->host = getenv('MYSQLHOST');
+            $this->user = getenv('MYSQLUSER');
+            $this->pwd = getenv('MYSQLPASSWORD');
+            $this->dbName = getenv('MYSQLDATABASE');
+            $this->port = getenv('MYSQLPORT') ?: 3306;
         } else {
             // Local development settings
-            $this->host = "localhost";
+            $this->host = "127.0.0.1";
             $this->user = "root";
             $this->pwd = "";
             $this->dbName = "hotel_db";
+            $this->port = 3306;
         }
     }
 
     protected function connect() {
-        $conn = new mysqli($this->host, $this->user, $this->pwd, $this->dbName);
+        $conn = new mysqli($this->host, $this->user, $this->pwd, $this->dbName, $this->port);
         
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
